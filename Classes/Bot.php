32ecebda078e6ffc1ddf53b4e6db01d719ce7c13
Noah Haupt <noahhaupt@Noahs-MacBook-Pro.local>
2023-09-16 11:07:29 -0400

    Updated the bot decision making

--- Classes/Bot.php
+++ Classes/Bot.php
@@ -4,7 +4,7 @@ require 'Player.php';
 
 class Bot extends Player 
 {
-    public int $passNum = 2;
+    public int $passNum = 3;
     public float $sleepTime = 3;
 
     public function evaluateRoll(array $rollData, Dice $dice)
@@ -39,8 +39,8 @@ class Bot extends Player
         // player scored all of the dice so we reset to 6 just in case they want to roll again
         if ($this->rollNum == 0) $this->rollNum = 6;
 
-        // see if the player wants to roll again or pass the turn
-        $passTurnDecision = ($this->rollNum > $this->passNum) ? 'no' : 'yes';
+        // see if the player wants to pass the turn
+        $passTurnDecision = ($this->rollNum <= $this->passNum || $this->bank >= 1000) ? 'yes' : 'no';
         echo "You currently have " . $this->bank . " points banked and " . $this->rollNum . " rollable dice. Would you like to pass the turn?: $passTurnDecision\n";
         sleep($this->sleepTime);
 
